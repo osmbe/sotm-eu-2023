975aca8bbce55841dbaf0c422220d7faaded2b56
Jonathan Beliën <dev@jbelien.be>
2023-04-18 15:20:29 +0200

    Initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import { Hero } from '@/components/Hero'
+import { Newsletter } from '@/components/Newsletter'
+import { Schedule } from '@/components/Schedule'
+import { Speakers } from '@/components/Speakers'
+import { Sponsors } from '@/components/Sponsors'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>DeceptiConf - A community-driven design conference</title>
+        <meta
+          name="description"
+          content="At DeceptiConf you’ll learn about the latest dark patterns being developed to trick even the smartest visitors, and you’ll learn how to deploy them without ever being detected."
+        />
+      </Head>
+      <Header />
+      <main>
+        <Hero />
+        <Speakers />
+        <Schedule />
+        <Sponsors />
+        <Newsletter />
+      </main>
+      <Footer />
+    </>
+  )
+}
