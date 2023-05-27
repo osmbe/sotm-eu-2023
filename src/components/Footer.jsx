99f9b47076fc48fd618d64e55ccc006709ab521a
Jonathan Beliën <dev@jbelien.be>
2023-05-27 16:57:42 +0200

    Fix random logo in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,19 @@
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
 import { Container } from '@/components/Container'
 import LogoBlue from '@/images/logos/SOTM_Blue_logo.png'
 import LogoYellow from '@/images/logos/SOTM_Yellow_logo.png'
 
 export function Footer() {
+  const [logo, setLogo] = useState(LogoBlue)
+
+  useEffect(() => Math.random() < 0.5 ? setLogo(LogoYellow) : setLogo(LogoBlue), [])
+
   return (
     <footer className="py-16">
       <Container className="flex flex-col items-center justify-between md:flex-row">
-        <Image src={Math.random() < 0.5 ? LogoBlue : LogoYellow} height={100} width={100} alt="SotM EU 2023 logo" className="h-12 w-auto text-slate-900" />
+        <Image src={logo} height={100} width={100} alt="SotM EU 2023 logo" className="h-12 w-auto text-slate-900" />
         <p className="mt-6 text-base text-slate-500 md:mt-0">
           Copyright &copy; State of the Map EU 2023 rights reserved.
         </p>
