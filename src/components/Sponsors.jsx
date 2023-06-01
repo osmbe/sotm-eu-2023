ce402c155ba896f1381397103ddacf100df0dfc1
xivk <ben@anyways.eu>
2023-06-01 12:21:37 +0200

    Added the confirmed sponsors.

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -11,10 +11,24 @@ import logoMeta from '@/images/sponsors/meta.svg'
 import logoYm from '@/images/sponsors/youthmappers.png'
 import logoMapbox from '@/images/sponsors/mapbox.svg'
 
-const sponsors = [
+const platinum = [
+  { name: 'TomTom', logo: logoTomTom },
+]
+
+const supporter = [
+  { name: 'Open Cage', website: "https://opencagedata.com/" },
+  { name: 'ANYWAYS', website: "https://www.anyways.eu/" },
+  { name: 'Stoomlink', website: "https://stoomlink.com/" },
+  { name: 'Champs Libres', website: "https://www.champs-libres.coop/" },
+]
+
+const gold = []
+const silver = []
+const bronze = []
+
+const pastSponsors = [
   { name: 'Next Billion', logo: logoNextBillion },
   { name: 'ESRI', logo: logoEsri },
-  { name: 'Tom Tom', logo: logoTomTom },
   { name: 'Grab', logo: logoGrab },
   { name: 'Microsoft', logo: logoMicrosoft },
   { name: 'Meta', logo: logoMeta },
@@ -29,15 +43,27 @@ export function Sponsors() {
         <h2 className="mx-auto max-w-2xl text-center font-dunbar text-4xl font-medium tracking-tighter text-sotm-blue sm:text-5xl">
           Our sponsors
         </h2>
-
         <div className="mx-auto mt-6 space-y-6 text-center text-2xl tracking-tight text-sotm-blue">
-          Want to sponsor? Check <Link className="font-bold" target="_blank" href="/pdf/SotmEU_Sponsorship_Packet.pdf">here</Link> for more info!
+          Want to sponsor too? Check <Link className="font-bold" target="_blank" href="/pdf/SotmEU_Sponsorship_Packet.pdf">here</Link> for more info!
         </div>
         <h3 className="mx-auto max-w-2xl text-center font-dunbar text-2xl font-semibold tracking-tight text-sotm-blue pt-8">
-          Some of the past State of the Map sponsors
+          Platinum
+        </h3>
+        <div className="mx-auto mt-8 grid max-w-max grid-cols-1 place-content-center gap-x-32 gap-y-12 sm:grid-cols-1 md:gap-x-16 lg:gap-x-32">
+          {platinum.map((sponsor) => (
+            <div
+              key={sponsor.name}
+              className="flex items-center justify-center"
+            >
+              <Image src={sponsor.logo} alt={sponsor.name} sizes="(max-width: 250px) 100vw" unoptimized />
+            </div>
+          ))}
+        </div>
+        {/* <h3 className="mx-auto max-w-2xl text-center font-dunbar text-2xl font-semibold tracking-tight text-sotm-blue pt-8">
+          Gold
         </h3>
         <div className="mx-auto mt-20 grid max-w-max grid-cols-1 place-content-center gap-x-32 gap-y-12 sm:grid-cols-4 md:gap-x-16 lg:gap-x-32">
-          {sponsors.map((sponsor) => (
+          {gold.map((sponsor) => (
             <div
               key={sponsor.name}
               className="flex items-center justify-center"
@@ -46,6 +72,58 @@ export function Sponsors() {
             </div>
           ))}
         </div>
+        <h3 className="mx-auto max-w-2xl text-center font-dunbar text-2xl font-semibold tracking-tight text-sotm-blue pt-8">
+          Silver
+        </h3>
+        <div className="mx-auto mt-20 grid max-w-max grid-cols-1 place-content-center gap-x-32 gap-y-12 sm:grid-cols-4 md:gap-x-16 lg:gap-x-32">
+          {silver.map((sponsor) => (
+            <div
+              key={sponsor.name}
+              className="flex items-center justify-center"
+            >
+              <Image src={sponsor.logo} alt={sponsor.name} sizes="(max-width: 150px) 100vw" unoptimized />
+            </div>
+          ))}
+        </div>
+        <h3 className="mx-auto max-w-2xl text-center font-dunbar text-2xl font-semibold tracking-tight text-sotm-blue pt-8">
+          Bronze
+        </h3>
+        <div className="mx-auto mt-20 grid max-w-max grid-cols-1 place-content-center gap-x-32 gap-y-12 sm:grid-cols-4 md:gap-x-16 lg:gap-x-32">
+          {bronze.map((sponsor) => (
+            <div
+              key={sponsor.name}
+              className="flex items-center justify-center"
+            >
+              <Image src={sponsor.logo} alt={sponsor.name} sizes="(max-width: 150px) 100vw" unoptimized />
+            </div>
+          ))}
+        </div> */}
+        <h3 className="mx-auto max-w-2xl text-center font-dunbar text-2xl font-semibold tracking-tight text-sotm-blue pt-8">
+          Supporter
+        </h3>
+        <div className="mx-auto mt-8 grid max-w-max grid-cols-1 place-content-center gap-x-32 gap-y-12 sm:grid-cols-4 md:gap-x-16 lg:gap-x-32  text-sotm-blue">
+          {supporter.map((sponsor) => (
+            <div
+              key={sponsor.name}
+              className="flex items-center justify-center"
+            >
+              <Link target="_blank" href={sponsor.website}>{sponsor.name}</Link>
+            </div>
+          ))}
+        </div>
+        {/* <h3 className="mx-auto max-w-2xl text-center font-dunbar text-2xl font-semibold tracking-tight text-sotm-blue pt-8">
+          Some of the past State of the Map sponsors
+        </h3>
+        <div className="mx-auto mt-20 grid max-w-max grid-cols-1 place-content-center gap-x-32 gap-y-12 sm:grid-cols-6 md:gap-x-16 lg:gap-x-32">
+          {pastSponsors.map((sponsor) => (
+            <div
+              key={sponsor.name}
+              className="flex items-center justify-center"
+            >
+              <Image src={sponsor.logo} alt={sponsor.name} sizes="(max-width: 50px) 100vw" unoptimized />
+            </div>
+          ))}
+        </div> */}
 
       </Container>
     </section>
