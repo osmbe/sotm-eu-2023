68e9d0846e9bf83c1e9e7082f04bec14fcf91d46
Jonathan Beliën <dev@jbelien.be>
2023-07-09 11:26:13 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
+import Link from 'next/link'
 
 import { Container } from '@/components/Container'
 import LogoBlue from '@/images/logos/SOTM_Blue_logo.png'
@@ -15,25 +16,38 @@ export function Footer() {
 
   return (
     <footer className="py-16">
-      <Container className="flex flex-col items-center justify-between md:flex-row">
-        <Image
-          src={logo}
-          height={100}
-          width={100}
-          alt="SotM EU 2023 logo"
-          className="h-12 w-auto text-slate-900"
-        />
-        <p className="mt-6 text-base text-slate-500 md:mt-0">
-          Copyright &copy; State of the Map EU 2023 rights reserved.
-        </p>
-        <a
-          rel="me"
-          href="https://en.osm.town/@sotmeu"
-          className="text-sotm-blue"
-          target="_blank"
-        >
-          Mastodon
-        </a>
+      <Container>
+        <div className="flex flex-col items-center justify-center md:flex-row">
+          <Link
+            href="/code-of-conduct"
+            className="text-sotm-blue"
+            target="_blank"
+          >
+            Code of Conduct
+          </Link>
+        </div>
+        <div className="flex flex-col items-center justify-between md:flex-row">
+          <Link href="/">
+          <Image
+            src={logo}
+            height={100}
+            width={100}
+            alt="SotM EU 2023 logo"
+            className="h-12 w-auto text-slate-900"
+          />
+          </Link>
+          <p className="mt-6 text-base text-slate-500 md:mt-0">
+            Copyright &copy; State of the Map EU 2023 rights reserved.
+          </p>
+          <a
+            rel="me"
+            href="https://en.osm.town/@sotmeu"
+            className="text-sotm-blue"
+            target="_blank"
+          >
+            Mastodon
+          </a>
+        </div>
       </Container>
     </footer>
   )
