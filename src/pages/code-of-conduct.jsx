9cce7d4bb4b675f4dd16c75fcfb1b55353cf7836
Jonathan Beliën <dev@jbelien.be>
2023-07-09 11:03:41 +0200

    Create Code of Conduct

---
+++ src/pages/code-of-conduct.jsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import { CodeOfConduct } from '@/components/CodeOfConduct'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>State of the Map EU 2023</title>
+        <meta
+          name="description"
+          content="State of the Map EU 2023 is the European conference of OpenStreetMap, organised this year by the OpenStreetMap Belgium community."
+        />
+      </Head>
+      <Header />
+      <main>
+        <CodeOfConduct id="codeofconduct" />
+      </main>
+      <Footer />
+    </>
+  )
+}
