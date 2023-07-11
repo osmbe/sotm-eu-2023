457bb930de09b655ad8dd6d65bc50ddcebc59c10
xivk <ben@anyways.eu>
2023-07-11 16:27:46 +0200

    Attempt to add contect to call for papers.

--- src/components/CallForPapers.jsx
+++ src/components/CallForPapers.jsx
@@ -9,16 +9,29 @@ export function CallForPapers({ id }) {
         <h2 className="mx-auto max-w-2xl text-center font-dunbar text-4xl font-medium tracking-tighter text-sotm-blue sm:text-5xl">
           Call for Papers
         </h2>
+
+        <h3 className="mx-auto mt-6 max-w-2xl text-center font-dunbar text-2xl font-medium tracking-tighter text-sotm-blue sm:text-3xl">
+          Submit your presentation
+        </h3>
+
         <div className="mx-auto mt-6 space-y-6 text-center font-poppins text-2xl tracking-tight text-sotm-blue">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-            mattis est quis molestie dictum. Vestibulum ante ipsum primis in
-            faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum
-            volutpat ac lectus at bibendum. Proin id metus vel nulla
-            sollicitudin lacinia vel vel justo. Nam dapibus justo neque, sed
-            posuere mi porta id. Donec at ultricies metus. Duis ac blandit diam,
-            ut cursus erat. Nam et orci aliquet, aliquam nisi faucibus, aliquam
-            lacus.
+            You're invited to propose a session. Whether you're new to OSM or an experienced contributor, we want to hear from you. Submit your session proposal today and share your mapping or technical expertise, discuss OpenStreetMap topics, and related content. We especially encourage local and regional mappers to participate.
+            Who should propose a session? You! Whether you're a seasoned speaker or a first-time proposer, we welcome your ideas. Share your expertise, lead a workshop or dive deep into OpenStreetMap-related discussions. State of the Map Europe is a platform where all voices are valued.
+
+          </p>
+          <h3 className="mx-auto mt-6 max-w-2xl text-center font-dunbar text-2xl font-medium tracking-tighter text-sotm-blue sm:text-3xl">
+            Timeline & Deadlines
+          </h3>
+          <p>
+            Grab this opportunity, contribute to an inspiring and collaborative conference and take the stages at State of the Map Europe 2023.
+            <ul>
+              <li>July 31: Call for Proposals Opens</li>
+              <li>September 4: Deadline for submissions</li>
+              <li>September 25: Speakers notified of final decision</li>
+              <li>October 16: Program Announced</li>
+              <li>November 10-12: State of the Map Europe 2023</li>
+            </ul>
           </p>
           <p>
             <Link
