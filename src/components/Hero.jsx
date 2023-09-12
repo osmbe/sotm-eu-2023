3d3f4aebb021786e54e4ab9ac5acd3cf53cd9c6d
Jonathan Beliën <dev@jbelien.be>
2023-09-12 21:49:37 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,7 +36,7 @@ export function Hero({ id }) {
 
   return (
     <div id={id} className="relative pb-20">
-      <div className="bg-indigo-50 absolute inset-x-0 -bottom-14 -top-48 overflow-hidden">
+      {/* <div className="bg-indigo-50 absolute inset-x-0 -bottom-14 -top-48 overflow-hidden">
         <Image
           className="absolute left-0 top-0 translate-x-[-55%] translate-y-[-10%] -scale-x-100 opacity-10 sm:left-1/2 sm:translate-x-[-98%] sm:translate-y-[-6%] lg:translate-x-[-106%] xl:translate-x-[-122%]"
           src={background}
@@ -48,7 +48,7 @@ export function Hero({ id }) {
         />
         <div className="absolute inset-x-0 top-0 h-40 bg-gradient-to-b from-white" />
         <div className="absolute inset-x-0 bottom-0 h-40 bg-gradient-to-t from-white" />
-      </div>
+      </div> */}
       <Container className="relative">
         <div className="mx-auto max-w-2xl lg:max-w-4xl lg:px-12">
           <h1 className="font-poppins text-5xl font-bold tracking-tighter text-sotm-blue sm:text-7xl">
