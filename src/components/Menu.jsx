6705da82c0c196621926421b7f69c3db28f6f5d1
nobohan <juminet@gmail.com>
2023-09-25 21:13:03 +0200

    Call for maps: add mobile support

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -49,7 +49,7 @@ export function Menu() {
           <Button href={ticketURL} target="_blank">
             Get your tickets
           </Button>
-          <Button href="/call-for-maps">Submit your maps</Button>
+          <Button className="hidden lg:block" href="/call-for-maps">Submit your maps</Button>
         </div>
         <div className="flex lg:hidden">
           <button
@@ -109,6 +109,12 @@ export function Menu() {
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Get your tickets
+                </Link>
+                <Link
+                  href="/call-for-maps"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Submit your maps
                 </Link>
               </div>
             </div>
