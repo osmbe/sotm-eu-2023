cd53e0c6b8ff3a4729bdc1f2df3fa9c1ed6337c4
Soroush <seyedsoroush.owji@tomtom.com>
2023-11-06 00:46:09 +0100

    fix page 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,19 +1,35 @@
 import { Container } from '@/components/Container'
+import Head from 'next/head'
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
 
-export function PageNotFound({ id }) {
+export default function Page() {
   return (
-    <section id={id} aria-label="Page not found" className="py-20 sm:py-32">
-      <Container>
-        <h2 className="mx-auto max-w-2xl text-center font-dunbar text-5xl font-extrabold tracking-tighter text-sotm-blue">
-          Page not found
-        </h2>
-        <div className="mt-6 space-y-6 text-center font-poppins text-2xl tracking-tight text-sotm-blue">
-          <p>
-            The page you are looking for does not exist. Please check the URL
-            and try again.
-          </p>
-        </div>
-      </Container>
-    </section>
+    <>
+      <Head>
+        <title>State of the Map EU 2023</title>
+        <meta
+          name="description"
+          content="State of the Map EU 2023 is the European conference of OpenStreetMap, organised this year by the OpenStreetMap Belgium community."
+        />
+      </Head>
+      <Header />
+      <main>
+        <section aria-label="Page not found" className="py-20 sm:py-32">
+          <Container>
+            <h2 className="mx-auto max-w-2xl text-center font-dunbar text-5xl font-extrabold tracking-tighter text-sotm-blue">
+              Page not found
+            </h2>
+            <div className="mt-6 space-y-6 text-center font-poppins text-2xl tracking-tight text-sotm-blue">
+              <p>
+                The page you are looking for does not exist. Please check the
+                URL and try again.
+              </p>
+            </div>
+          </Container>
+        </section>
+      </main>
+      <Footer />
+    </>
   )
 }
